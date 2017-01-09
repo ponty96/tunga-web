14c16c90f616bff3239e8345862823428d3a3910
Ayomide Aregbede <ayo.aregbede@gmail.com>
2017-01-09 14:06:27 +0100

    more refactoring

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 import Progress from './status/Progress';
 import LoadMore from './status/LoadMore';
 import UserCard from './UserCard';
-import SearchBox from './SearchBox';
 
 export default class UserList extends React.Component {
 
+    static propTypes = {
+        Auth: PropTypes.object.isRequired,
+        User: PropTypes.object.isRequired,
+        updateConnection: PropTypes.func.isRequired,
+        deleteConnection: PropTypes.func.isRequired,
+        createConnection: PropTypes.func.isRequired,
+        filter: PropTypes.string.isRequired
+    }
     render() {
         const {
             Auth,
@@ -14,82 +21,27 @@ export default class UserList extends React.Component {
             updateConnection,
             deleteConnection,
             createConnection,
-            listUsers,
-            listMoreUsers,
-            filter,
-            skill,
-            requests} = this.props;
+            filter} = this.props;
 
         return (
             <div>
-                {this.props.hide_header ? null : (
-                    <div>
-                        <div className="clearfix">
-                            <div className="pull-right">
-                                <SearchBox placeholder="Search by name or skills"
-                                    filter={{ filter, skill }}
-                                    onSearch={listUsers}
-                                    count={User.list.count} />
+                <div className="row flex-row">
+                    {User.list.ids.map((id) => {
+                        const user = User.list.users[id];
+                        return (
+                            <div className="col-sm-6 col-md-4" key={id}>
+                                <UserCard Auth={Auth} user={user}
+                                    deleteConnection={deleteConnection}
+                                    createConnection={createConnection}
+                                    updateConnection={updateConnection}
+                                    hideOnDisconnect={filter == 'team'} />
                             </div>
-                            <h2 className="pull-left">People</h2>
-                        </div>
-                        <div className="nav-top-filter">
-                            <Link to="/people/filter/developers" activeClassName="active">Coders</Link>
-                            {Auth.isAuthenticated ? (
-                                [
-                                    Auth.user.is_staff ? (
-                                        <Link to="/people/filter/clients" key="clients"
-                                            activeClassName="active">Clients</Link>) : null,
-                                    <Link to="/people/filter/team" activeClassName="active" key="team">
-                                        {Auth.user.is_developer ? 'My friends' : 'My team'}
-                                    </Link>,
-                                    Auth.user.is_developer ? (
-                                        <Link to="/people/filter/my-clients" key="my-clients" activeClassName="active">My Clients</Link>
-                                    ) : null,
-                                    <Link to="/people/filter/requests" activeClassName="active" key="requests"
-                                        style={{ marginLeft: '20px' }}>
-                                        Requests {requests ? <span className="badge">{requests}</span> : null}
-                                    </Link>,
-                                    Auth.user.is_project_owner || Auth.user.is_staff ? (
-                                        <Link to="/people/filter/relevant" key="relevant" activeClassName="active">Relevant to
-                                            me</Link>
-                                    ) : null
-                                ]
-                            ) : null}
-                            {skill ? (
-                                <Link to={`/people/skill/${skill}/`} className="active" style={{ marginLeft: '20px' }}>
-                                    <i className="tunga-icon-tag" /> {skill}
-                                </Link>
-                            ) : null}
-                        </div>
-                    </div>
-                )}
-                {User.list.isFetching ?
-                    (<Progress />)
-                    :
-                    (<div>
-                        <div className="row flex-row">
-                            {User.list.ids.map((id) => {
-                                const user = User.list.users[id];
-                                return (
-                                    <div className="col-sm-6 col-md-4" key={id}>
-                                        <UserCard Auth={Auth} user={user}
-                                            deleteConnection={deleteConnection}
-                                            createConnection={createConnection}
-                                            updateConnection={updateConnection}
-                                            hideOnDisconnect={filter == 'team'} />
-                                    </div>
-                                );
-                            })}
-                        </div>
-                        <LoadMore url={User.list.next} callback={listMoreUsers}
-                            loading={User.list.isFetchingMore} />
-                        {User.list.ids.length ? null : (
-                            <div className="alert alert-info">No users match your query</div>
-                        )}
-                    </div>)
-                }
+                        );
+                    })}
+                </div>
             </div>
         );
     }
 }
+
+
--- src/containers/UserContainer.jsx
+++ src/containers/UserContainer.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import UserList from './../components/UserList';
+import SearchBox from './../components/SearchBox';
+import { Link, IndexLink } from 'react-router';
+import LoadMore from './../components/status/LoadMore';
+import Progress from './../components/status/Progress';
 
 export default class UserContainer extends React.Component {
+  static propTypes = {
+        Auth: PropTypes.object.isRequired,
+        User: PropTypes.object.isRequired,
+        UserActions: PropTypes.shape({
+          updateConnection: PropTypes.func.isRequired,
+          deleteConnection: PropTypes.func.isRequired,
+          createConnection: PropTypes.func.isRequired, 
+          listUsers: PropTypes.func.isRequired,
+          listMoreUsers: PropTypes.func.isRequired,
+        })
+    }
+
   componentDidMount() {
     this.props.UserActions.listUsers({
       filter: this.getFilter(),
@@ -49,18 +65,92 @@ export default class UserContainer extends React.Component {
     let skill = this.getSkill();
     return (
       <div>
-        <UserList
-          updateConnection={updateConnection}
-          deleteConnection={deleteConnection}
-          createConnection={createConnection}
-          listUsers={listUsers}
-          listMoreUsers={listMoreUsers}
-          filter={filter}
-          skills={skill}
-          request={requests}
-          Auth={Auth}
-          User={User} />
+        {this.props.hide_header ? null : (
+          <div>
+            <div className="clearfix">
+              <div className="pull-right">
+                <SearchBox placeholder="Search by name or skills"
+                  filter={{ filter, skill }}
+                  onSearch={listUsers}
+                  count={User.list.count} />
+              </div>
+              <h2 className="pull-left">People</h2>
+            </div>
+            <div className="nav-top-filter">
+              <Link to="/people/filter/developers" activeClassName="active">Coders</Link>
+              {Auth.isAuthenticated ? (
+                [
+                  Auth.user.is_staff ? (
+                    <Link to="/people/filter/clients" key="clients"
+                      activeClassName="active">Clients</Link>) : null,
+                  <Link to="/people/filter/team" activeClassName="active" key="team">
+                    {Auth.user.is_developer ? 'My friends' : 'My team'}
+                  </Link>,
+                  Auth.user.is_developer ? (
+                    <Link to="/people/filter/my-clients" key="my-clients" activeClassName="active">My Clients</Link>
+                  ) : null,
+                  <Link to="/people/filter/requests" activeClassName="active" key="requests"
+                    style={{ marginLeft: '20px' }}>
+                    Requests {requests ? <span className="badge">{requests}</span> : null}
+                  </Link>,
+                  Auth.user.is_project_owner || Auth.user.is_staff ? (
+                    <Link to="/people/filter/relevant" key="relevant" activeClassName="active">Relevant to
+                                            me</Link>
+                  ) : null
+                ]
+              ) : null}
+              {skill ? (
+                <Link to={`/people/skill/${skill}/`} className="active" style={{ marginLeft: '20px' }}>
+                  <i className="tunga-icon-tag" /> {skill}
+                </Link>
+              ) : null}
+            </div>
+          </div>
+        )}
+        <div>
+          {this.renderUserList()}
+        </div>
       </div>
     );
+  }
+
+  renderUserList = () => {
+    const {
+      Auth,
+      User,
+      updateConnection,
+      deleteConnection,
+      createConnection,
+      listMoreUsers,
+      filter} = this.props;
+
+    if (User.list.isFetching) {
+      return <Progress />
+    } else {
+      return (
+        <div>
+          <UserList
+            updateConnection={updateConnection}
+            deleteConnection={deleteConnection}
+            createConnection={createConnection}
+            Auth={Auth}
+            User={User}
+            filter={filter} />
+          <LoadMore url={User.list.next} callback={listMoreUsers}
+            loading={User.list.isFetchingMore} />
+          {User.list.ids.length ? null : (
+            <div className="alert alert-info">No users match your query</div>
+          )}
+        </div>
+      )
+
+    }
+  }
+
+  getUsers = (list) => {
+    if (list.count > 0 && list.count != null) {
+      return list.id.map(id => (list.users[id]))
+    }
+    return []
   }
 }
